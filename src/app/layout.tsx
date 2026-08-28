fa361fac786788f46f8054b2a3ff23b5962775df
Lars Kvihaugen <lars@kvihaugen.no>
2026-08-28 18:08:48 +0200

    [dev]: reconfigure css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../css/common.css";
 import ParentProps from "@/types/common/ParentProps";
 import { DESCRIPTION, NAME } from "@/globals/metadata.globals";
 
